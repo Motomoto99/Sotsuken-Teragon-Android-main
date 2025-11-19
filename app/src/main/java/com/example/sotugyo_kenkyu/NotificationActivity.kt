201c1399b9bfc79224836a17382a8b6ab7951a12
221083 <221083@std.hi-joho.ac.jp>
2025-11-19 12:16:56 +0900

    お知らせ画面追加

---
+++ app/src/main/java/com/example/sotugyo_kenkyu/NotificationActivity.kt
@@ -0,0 +1,29 @@
+package com.example.sotugyo_kenkyu
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_notification)
+
+        // ステータスバーの余白調整
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 戻るボタンを押したら画面を閉じる
+        val backButton = findViewById<ImageButton>(R.id.buttonBack)
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
